08ef2d514de16a1a88fc42b4bd8e0f240188543e
Erik Zhao <erikzhao98@gmail.com>
2020-07-29 21:34:27 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,6 +1,6 @@
 def nyc_pigeon_organizer(data)
   # write your code here
-  final_output = data.each_with_objec({}) do |(key, value), final_array|
+  final_output = data.each_with_object({}) do |(key, value), final_array|
     value.each do |inner_key, names|
       names.each do |name|
         if !final_array[name]
